f84e13e766594b77f8a0d16efbef78d2ded78e25
shahabullah2304 <shahabullah990@gmail.com>
2023-11-13 22:50:40 +0500

    initial

---
+++ index.js
@@ -0,0 +1,16 @@
+let MyName= "Shahab ullah";
+let MyEmail="shahabullah990@gmail.com";
+let MyQualification="BSSE";
+let MyEducation="i am a software Engineer";
+let MyGender="Male";
+let MyExperiences="4 years of MyExperiences";
+let MyAge="22";
+let MyUniversity="Brains University";
+let MyBirthplace="Dir lower maidan shahbikey";
+let MyWeights="60";
+let MyHeighr="5.3";
+let MyOrganization="software company";
+let MyProfession="Software Engineer";
+
+
+console.log('');
